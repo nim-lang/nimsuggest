36e44eb52ec649310f0ce6b3321ff3015982f31f
Andreas Rumpf <rumpf_a@web.de>
2016-08-26 20:33:28 +0200

    first test works with the tester

--- tester.nim
+++ tester.nim
@@ -27,7 +27,7 @@ proc parseTest(filename: string): Test =
   for x in lines(filename):
     let marker = x.find(cursorMarker)+1
     if marker > 0:
-      markers.add dest & ":" & $i & ":" & $marker
+      markers.add filename & ";" & dest & ":" & $i & ":" & $marker
       tmp.writeLine x.replace(cursorMarker, "")
     else:
       tmp.writeLine x
@@ -35,13 +35,14 @@ proc parseTest(filename: string): Test =
       inc specSection
     elif specSection == 1:
       if x.startsWith("$nimsuggest"):
-        result.cmd = x % ["nimsuggest", nimsug, "file", dest]
+        result.cmd = x % ["nimsuggest", nimsug, "file", filename]
       elif x.startsWith(">"):
         # since 'markers' here are not complete yet, we do the $substitutions
         # afterwards
         result.script.add((x.substr(1), ""))
       else:
         # expected output line:
+        let x = x % ["file", filename]
         result.script[^1][1].add x.replace(";;", "\t") & '\L'
     inc i
   tmp.close()
@@ -49,7 +50,7 @@ proc parseTest(filename: string): Test =
   for a in mitems(result.script):
     a[0] = a[0] % markers
 
-proc runTest(filename: string) =
+proc runTest(filename: string): int =
   let s = parseTest filename
   let cl = parseCmdLine(s.cmd)
   var p = startProcess(command=cl[0], args=cl[1 .. ^1],
@@ -59,30 +60,36 @@ proc runTest(filename: string) =
   let inp = p.inputStream
   var report = ""
   var a = newStringOfCap(120)
-  # read and ignore anything nimsuggest says at startup:
-  while outp.readLine(a):
-    echo "got ", a
-    if a == DummyEof: break
-  for req, resp in items(s.script):
-    echo "requesting ", req
-    inp.writeLine(req)
-    var answer = ""
-    when false:
+  try:
+    # read and ignore anything nimsuggest says at startup:
+    while outp.readLine(a):
+      if a == DummyEof: break
+    for req, resp in items(s.script):
+      inp.writeLine(req)
+      inp.flush()
+      var answer = ""
       while outp.readLine(a):
         if a == DummyEof: break
         answer.add a
         answer.add '\L'
-    else:
-      answer = outp.readAll()
-    if answer != resp:
-      report.add "\nTest failed: " & filename
-      report.add "\n  Expected:  " & resp
-      report.add "\n  But got:   " & answer
-  inp.writeLine("quit")
-  close(p)
+      if answer != resp:
+        report.add "\nTest failed: " & filename
+        report.add "\n  Expected:  " & resp
+        report.add "\n  But got:   " & answer
+  finally:
+    inp.writeLine("quit")
+    inp.flush()
+    close(p)
+  if report.len > 0:
+    echo report
+  result = report.len
 
 proc main() =
+  var failures = 0
   for x in walkFiles("tests/t*.nim"):
-    runTest(x)
+    echo "Test ", x
+    failures += runTest(expandFilename(x))
+  if failures > 0:
+    quit 1
 
 main()
--- tests/tdot1.nim
+++ tests/tdot1.nim
@@ -1,9 +1,9 @@
 discard """
 $nimsuggest --tester $file
 >sug $1
-sug;;skField;;x;;int;;/private/tmp/tdot1.nim;;10;;4;;"";;100
-sug;;skField;;y;;int;;/private/tmp/tdot1.nim;;10;;7;;"";;100
-sug;;skProc;;tdot1.main;;proc (f: Foo);;/private/tmp/tdot1.nim;;12;;5;;"";;100
+sug;;skField;;x;;int;;$file;;11;;4;;"";;100
+sug;;skField;;y;;int;;$file;;11;;7;;"";;100
+sug;;skProc;;tdot1.main;;proc (f: Foo);;$file;;13;;5;;"";;100
 """
 
 type
